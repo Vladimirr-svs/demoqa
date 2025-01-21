bbd3196bb8687847e6979748f911eef9909d9bda
Vladimir <s.v.s.78@yandex.ru>
2025-01-21 19:14:40 +0300

    second commit

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -1,10 +1,10 @@
 from pages.demoqa import DemoQa
-import time
+
 
 def test_chesk_icon(browser):
     demo_qa_page = DemoQa(browser)
     demo_qa_page.visit()
-    demo_qa_page.click_on_the_icon()
+    demo_qa_page.icon.click()
     assert demo_qa_page.equal_url()
     assert demo_qa_page.exist_icon()
 

8f17f20b017e521ded7b8875b531964d072552f3
Vladimirr-svs <s.v.s.78@yandex.ru>
2025-01-22 22:11:02 +0300

    Add files via upload

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,4 +1,5 @@
 from pages.base_page import BasePage
+from components.components import WebElement
 
 class ElementsPage(BasePage):
 
@@ -6,3 +7,5 @@ class ElementsPage(BasePage):
     def __init__(self, driver):
         self.base_url = 'https://demoqa.com/elements'
         super().__init__(driver, self.base_url)
+
+        self.text_please = WebElement(driver, '#app > div > div > div > div.col-12.mt-4.col-md-6')
